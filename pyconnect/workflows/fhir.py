e9c3e829413b6cea88147b497576e81b8e71b9c8
ccorley <ccorley@us.ibm.com>
2021-02-18 13:01:04 -0600

    Update fhir.py

--- pyconnect/workflows/fhir.py
+++ pyconnect/workflows/fhir.py
@@ -5,12 +5,6 @@ class FhirWorkflow(core.CoreWorkflow):
     """
     Implements a FHIR validation and storage workflow for LinuxForHealth.
     """
-
-    def __init__(self, message):
-        self.message = message
-
-    state = core.CoreWorkflowDef()
-
     @xworkflows.transition('do_validate')
     def validate(self):
         # Overridden to send message to a NATS subscriber for FHIR validation
