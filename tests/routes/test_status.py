5cf08040546e86cb2aee09dfbce299ae0271d051
Dixon Whitmire <dixonwh@gmail.com>
2021-03-12 22:05:22 -0500

    migrate server availability check to async

--- tests/routes/test_status.py
+++ tests/routes/test_status.py
@@ -2,39 +2,38 @@
 test_status.py
 Tests the /status API endpoints
 """
-import socket
 import pytest
-from pyconnect.config import get_settings
+from unittest.mock import AsyncMock
+from pyconnect.routes import status
 
 
 @pytest.mark.asyncio
-def test_status_get(test_client, settings, mock_client_socket, monkeypatch):
+async def test_status_get(async_test_client, monkeypatch):
     """
     Tests /status [GET]
-    :param test_client: Fast API test client
+    :param async_test_client: Fast API async test client
     :param settings: Settings test fixture
-    :param mock_client_socket: Mock Client Socket Fixture
     :param monkeypatch: MonkeyPatch instance used to mock test cases
     """
-    test_client.app.dependency_overrides[get_settings] = lambda: settings
-
     with monkeypatch.context() as m:
-        m.setattr(socket, 'socket', mock_client_socket)
+        m.setattr(status, 'is_service_available', AsyncMock(return_value=True))
 
-        actual_response = test_client.get('/status')
-        assert actual_response.status_code == 200
+        async with async_test_client as ac:
+            actual_response = await ac.get('/status')
 
-        actual_json = actual_response.json()
-        assert 'application_version' in actual_json
-        assert 'elapsed_time' in actual_json
-        assert actual_json['elapsed_time'] > 0.0
+            assert actual_response.status_code == 200
 
-        expected = {
-            'application': 'pyconnect.main:app',
-            'application_version': actual_json['application_version'],
-            'is_reload_enabled': False,
-            'nats_status': 'AVAILABLE',
-            'kafka_broker_status': 'AVAILABLE',
-            'elapsed_time': actual_json['elapsed_time']
-        }
-        assert actual_json == expected
+            actual_json = actual_response.json()
+            assert 'application_version' in actual_json
+            assert 'elapsed_time' in actual_json
+            assert actual_json['elapsed_time'] > 0.0
+
+            expected = {
+                'application': 'pyconnect.main:app',
+                'application_version': actual_json['application_version'],
+                'is_reload_enabled': False,
+                'nats_status': 'AVAILABLE',
+                'kafka_broker_status': 'AVAILABLE',
+                'elapsed_time': actual_json['elapsed_time']
+            }
+            assert actual_json == expected
---
+++ tests/support/test_availability.py
@@ -0,0 +1,78 @@
+"""
+test_availability.py
+
+Tests pyConnect service/host availability functions
+"""
+import asyncio
+from unittest.mock import AsyncMock
+import pytest
+from pyconnect.support.availability import (get_host_ports,
+                                            is_service_available,
+                                            ping_host)
+
+
+@pytest.mark.asyncio
+def test_ping_host(monkeypatch):
+    """
+    Validates positive and negative results for ping_host.
+    The underlying function used to "ping" the host, asyncio.open_connection,
+    is mocked for the positive and negative test cases.
+    :param monkeypatch:
+    """
+    with monkeypatch.context() as m:
+        mock = AsyncMock(return_value=(AsyncMock(), AsyncMock()))
+        m.setattr(asyncio, 'open_connection', mock)
+        actual_result = asyncio.run(ping_host('some-server', 8080))
+        assert actual_result is True
+
+        mock = AsyncMock(side_effect=OSError)
+        m.setattr(asyncio, 'open_connection', mock)
+        actual_result = asyncio.run(ping_host('some-server', 8080))
+        assert actual_result is False
+
+
+def test_get_host_ports():
+    """
+    Validates that host and port data is parsed from settings
+    """
+    service_setting = ['localhost:9090']
+    expected = [('localhost', 9090)]
+    actual = get_host_ports(service_setting)
+    assert actual == expected
+
+    service_setting = ['localhost']
+    expected = [('localhost', None)]
+    actual = get_host_ports(service_setting)
+    assert actual == expected
+
+    service_setting = ['localhost:9090', 'localhost:9091']
+    actual = get_host_ports(service_setting)
+    expected = [('localhost', 9090), ('localhost', 9091)]
+    assert actual == expected
+
+    service_setting = ['localhost:9090',' localhost:9091']
+    actual = get_host_ports(service_setting)
+    expected = [('localhost', 9090), ('localhost', 9091)]
+    assert actual == expected
+
+
+@pytest.mark.asyncio
+def test_is_service_available(monkeypatch):
+    """
+    Validates that is_service_available returns True for valid connections, False when an error occurs.
+    The underlying function used to confirm availability, asyncio.open_connection,
+    is mocked for the positive and negative test cases.
+    """
+    with monkeypatch.context() as m:
+
+        mock = AsyncMock(return_value=(AsyncMock(), AsyncMock()))
+        m.setattr(asyncio, 'open_connection', mock)
+        service_setting = ['https://localhost:8080', 'tls://otherhost:8080']
+        actual_result = asyncio.run(is_service_available(service_setting))
+        assert actual_result is True
+
+        mock = AsyncMock(side_effect=OSError)
+        m.setattr(asyncio, 'open_connection', mock)
+        service_setting = ['https://localhost:9090', 'tls://otherhost:8080']
+        actual_result = asyncio.run(is_service_available(service_setting))
+        assert actual_result is False
