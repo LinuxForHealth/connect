73b8fb4a4ca69376957f18ca66a457b43952d481
Dixon Whitmire <dixonwh@gmail.com>
2021-04-28 22:11:30 -0400

    renamed local-certs to local-config

--- local-config/install-certificates.sh
+++ local-config/install-certificates.sh
@@ -6,24 +6,24 @@
 # - Connect development certificate
 # - NATS development certificate
 
-SCRIPT_DIRECTORY=$(dirname "$0")
+OUTPUT_DIRECTORY=$(dirname "$0")/certs
 
 echo "install CA Root certificate"
 mkcert -install
 echo ""
 
-echo "copying rootCA.pem to $SCRIPT_DIRECTORY"
+echo "copying rootCA.pem to $OUTPUT_DIRECTORY"
 echo ""
 CA_ROOT_LOCATION=$(mkcert -CAROOT)
-cp "$CA_ROOT_LOCATION"/rootCA.pem "$SCRIPT_DIRECTORY"
+cp "$CA_ROOT_LOCATION"/rootCA.pem "$OUTPUT_DIRECTORY"
 
 echo "create NATS development certificate"
 echo ""
-mkcert -cert-file "$SCRIPT_DIRECTORY"/nats-server.pem \
-       -key-file "$SCRIPT_DIRECTORY"/nats-server.key \
+mkcert -cert-file "$OUTPUT_DIRECTORY"/nats-server.pem \
+       -key-file "$OUTPUT_DIRECTORY"/nats-server.key \
        nats-server connect_nats-server_1 localhost 127.0.0.1 ::1 \
 
 echo "create LinuxForHealth development certificate"
-mkcert -cert-file "$SCRIPT_DIRECTORY"/lfh.pem \
-       -key-file "$SCRIPT_DIRECTORY"/lfh.key \
+mkcert -cert-file "$OUTPUT_DIRECTORY"/lfh.pem \
+       -key-file "$OUTPUT_DIRECTORY"/lfh.key \
        lfh compose_lfh_1 localhost 127.0.0.1 ::1 \
