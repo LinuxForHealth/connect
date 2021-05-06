ebb754fa5e37dcdaba93898fda6d6345c17072da
Dixon Whitmire <dixonwh@gmail.com>
2021-05-06 05:27:26 -0400

    updating conftest formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,7 +33,9 @@ def settings() -> Settings:
 
 @pytest.fixture
 def lfh_data_record():
-    """ A LFH Data Record Fixture"""
+    """
+    :return: LFH Data Record Fixture
+    """
     return {
         "uuid": "dbe0e8dd-7b64-4d7b-aefc-d27e2664b94a",
         "creation_date": "2021-02-12T18:13:17Z",
@@ -94,7 +96,9 @@ def mock_async_kafka_producer() -> Callable:
             pass
 
         async def produce_with_callback(self, topic, value, on_delivery):
-            """is successful for any topic and value using any producer callback"""
+            """
+            Implements a producer with a callback
+            """
 
             class CallbackMessage:
                 """ Message to send to producer callback"""
