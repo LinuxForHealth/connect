a185b1cb291b93f48c2118dd33f051ca4b83df39
ccorley <ccorley@us.ibm.com>
2021-09-04 09:25:03 -0500

    Added IBM fhir as transmit server to config.py

--- connect/config.py
+++ connect/config.py
@@ -52,7 +52,9 @@ class Settings(BaseSettings):
     connect_logging_config_path: str = "logging.yaml"
     # Optional list of external FHIR server URLs to transmit to
     # Example: ['https://fhiruser:change-password@localhost:9443/fhir-server/api/v4']
-    connect_external_fhir_servers: List[str] = []
+    connect_external_fhir_servers: List[str] = [
+        "https://fhiruser:change-password@localhost:9443/fhir-server/api/v4"
+    ]
     connect_rate_limit: str = "5/second"
     connect_timing_enabled: bool = False
 
